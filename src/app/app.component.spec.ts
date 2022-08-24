fb384aaccf22f749ec124438d7665451110a31b4
Clément Begnaud <clement.begnaud@gmail.com>
2022-08-24 15:12:14 +0000

    pseudo perf tests sur TUs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,35 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+const componentCount = 1000;
+const testCount = 1;
 
-  it(`should have as title 'gitpod-fiddling'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gitpod-fiddling');
-  });
+for(let i = 0; i < componentCount; i++) {
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gitpod-fiddling app is running!');
+  class ComponentStub extends AppComponent {}
+
+  describe(`[#${i}] ComponentStub`, () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule
+        ],
+        declarations: [
+          AppComponent,
+          ComponentStub
+        ],
+      }).compileComponents();
+    });
+
+    for(let i = 0; i < testCount; i++) {
+      it('should create the app', () => {
+        const fixture = TestBed.createComponent(ComponentStub);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+      });
+    }
   });
-});
+}
